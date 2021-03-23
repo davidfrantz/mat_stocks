2ffecb73cac4ccd6cdb50c77bb3e963df7192894
Franz Schug <franz.schug@posteo.de>
2021-03-23 09:39:07 +0100

    Update zonal_stats_from_tiles.py

--- stock-mapping/type2/in/zonal_stats_from_tiles.py
+++ stock-mapping/type2/in/zonal_stats_from_tiles.py
@@ -4,14 +4,21 @@ import sys
 import gdal
 import numpy as np
 
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
+#print('Number of arguments:', len(sys.argv), 'arguments.')
+#print('Argument List:', str(sys.argv))
 
-targetPath = str(sys.argv[1]).split(" ")[0]
-zonesPath = str(sys.argv[2]).split(" ")[0]
+outPath = str(sys.argv[1]).split(" ")[0]
+targetPath = str(sys.argv[2]).split(" ")[0]
+zonesPath = str(sys.argv[3]).split(" ")[0]
 
-zones = gdal.Open(zonesPath).ReadAsArray()
-target = gdal.Open(stocksPath).ReadAsArray()
+targetDS = gdal.Open(targetPath)
+zonesDS = gdal.Open(zonesPath)
+
+targetND = targetDS.GetRasterBand(1).GetNoDataValue()
+#zonesND = zonesDS.GetRasterBand(1).GetNoDataValue()
+
+target = targetDS.ReadAsArray()
+zones = zonesDS.ReadAsArray()
 
 ## ID unique ids in zones, write to array
 uids = np.unique(zones)
@@ -19,9 +26,9 @@ sums = []
 
 ## loop through ids, for each id, build sum of target[zones == ID)
 for uid in uids:
-    sums.append(np.sum(target, where = zones == uid))
+    sums.append(np.sum(target, where = (zones == uid) & (target != targetND)))
 
 all = [uids, sums]
 
 ## write array to text file
-np.savetxt(targetPath + "/sum_.txt", np.transpose(all), delimiter=",", fmt='%1.2f')
+np.savetxt(outPath, np.transpose(all), delimiter=";", fmt='%1.2f')
