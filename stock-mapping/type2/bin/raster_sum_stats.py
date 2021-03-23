965072c8dad5b3747bd6e4a8101bd188bed5a490
Franz Schug <franz.schug@posteo.de>
2021-03-23 12:58:09 +0100

    Update raster_sum_stats.py

--- stock-mapping/type2/bin/raster_sum_stats.py
+++ stock-mapping/type2/bin/raster_sum_stats.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
 import os
 import sys
 import pandas as pd
 import numpy as np
+import csv
 
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
@@ -12,9 +13,10 @@ outPath = str(sys.argv[1]).split(" ")[0]
 
 sums = []
 for i in range(2,len(sys.argv)):
-    df = pd.read_csv(sys.argv[i], header = None)
+    df = pd.read_csv(sys.argv[i], header = None, sep=";")
     sums.append(df)
   
 frame = pd.concat(sums, axis=0, ignore_index = True)
-groupedDF = frame.groupby(0).sum()  
-groupedDF.to_csv(outPath, sep=";", header=False)
+frame.columns = ["zone","sum"]
+groupedDF = frame.groupby("zone").sum()
+groupedDF.to_csv(outPath, sep=";", header=True, quoting = csv.QUOTE_NONE)
