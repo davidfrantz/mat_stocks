2313b83022da47735085688ad85f374a6d0ad791
Franz Schug <franz.schug@posteo.de>
2021-03-23 09:39:04 +0100

    Update raster_sum_stats.py

--- stock-mapping/type2/in/raster_sum_stats.py
+++ stock-mapping/type2/in/raster_sum_stats.py
@@ -8,13 +8,14 @@ import numpy as np
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
 
+outPath = str(sys.argv[1]).split(" ")[0]
+
 sums = []
-for i in range(1,len(sys.argv)):
-    f.append(sys.argv[i])
-    
+for i in range(2,len(sys.argv)):
     df = pd.read_csv(sys.argv[i], header = None)
     sums.append(df)
-
+  
 frame = pd.concat(sums, axis=0, ignore_index = True)
 groupedDF = frame.groupby(0).sum()  
-groupedDF.to_csv("/data/Jakku/usa/counties/stats/US_" + state + "/US_AL.txt", sep=",", header=False)
+exit()
+groupedDF.to_csv(outPath, sep=";", header=False)
