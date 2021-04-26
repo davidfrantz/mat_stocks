6ef092635216ead2b3a693ffcbfaf8e36e6a965b
davidfrantz <frantz.feut@gmail.com>
2021-04-26 09:55:59 +0200

    updated scripts

---
+++ stock-mapping/writecsv_zonal.r
@@ -0,0 +1,34 @@
+require(dplyr)
+require(tidyr)
+
+cnt <- "USA"
+dbase <- "/data/Jakku/mat_stocks"
+dstock <- sprintf("%s/stock/%s/ALL", dbase, cnt)
+dcsv <- "/home/frantzda/mat_stocks/paper/USA/csv"
+
+files <-    dstock %>%
+            dir(".csv", full.names = TRUE, recursive = TRUE) %>%
+            grep("total", ., value = TRUE)
+nfiles <-   length(files)
+values <-   lapply(files,
+                   function(x) read.csv(x, sep = ";"))
+rfiles <-   files %>% gsub(dstock, "", .)
+labels <-   basename(files) %>%
+            gsub("\\..*", "", .) %>%
+            gsub("_total", "", .)
+
+df <- values[[1]]
+for (i in 2:nfiles) {
+    df <- df %>% 
+        full_join(values[[i]], by = "zone")
+}
+
+str(df)
+colnames(df) <- c("zone", labels)
+
+write.csv( df,
+           sprintf("%s/mass-per-county/zonal_mass_ENLOCALE.csv", dcsv),
+           row.names = FALSE)
+write.csv2(df,
+           sprintf("%s/mass-per-county/zonal_mass_DELOCALE.csv", dcsv),
+           row.names = FALSE)
--- stock-mapping/writecsvs.r
+++ stock-mapping/writecsvs.r
@@ -4,13 +4,13 @@ require(tidyr)
 cnt <- "USA"
 dbase <- "/data/Jakku/mat_stocks"
 dstock <- sprintf("%s/stock/%s", dbase, cnt)
-
+dcsv <- "/home/frantzda/mat_stocks/paper/USA/csv"
 
 files <-    dstock %>%
-            dir(".txt", full.names = TRUE, recursive = TRUE) %>%
+            dir(".csv", full.names = TRUE, recursive = TRUE) %>%
             grep("mosaic", ., value = TRUE)
-values <-   sapply(files, readLines) %>%
-            as.numeric()
+values <-   sapply(files,
+                   function(x) read.csv(x, sep = ";")$sum %>% sum())
 rfiles <-   files %>% gsub(dstock, "", .)
 
 df <- data.frame(
@@ -23,7 +23,7 @@ df <- data.frame(
 
 
 
-tmp <- rfiles %>% basename() %>% gsub("\\..*", "", .) 
+tmp <- rfiles %>% basename() %>% gsub("\\..*", "", .)
 for (i in unique(df$dimension)) tmp <- gsub(sprintf("^%s_", i), "", tmp)
 for (i in unique(df$material))  tmp <- gsub(sprintf("_%s$", i), "", tmp)
 df$category <- tmp
@@ -36,8 +36,8 @@ total_mass_all_states <- df %>%
                         pivot_wider(names_from = category, values_from = value)
 
 
-write.csv (total_mass_all_states,  sprintf("%s/total_mass_all_states_ENLOCALE.csv", dstock), row.names = FALSE)
-write.csv2(total_mass_all_states,  sprintf("%s/total_mass_all_states_DELOCALE.csv", dstock), row.names = FALSE)
+write.csv (total_mass_all_states,  sprintf("%s/mass-all-states/total_mass_all_states_ENLOCALE.csv", dcsv), row.names = FALSE)
+write.csv2(total_mass_all_states,  sprintf("%s/mass-all-states/total_mass_all_states_DELOCALE.csv", dcsv), row.names = FALSE)
 
 
 
@@ -48,8 +48,8 @@ write_state_csv <- function(df, key) {
         select(material, category, value) %>%
         pivot_wider(names_from = material, values_from = value)
 
-    write.csv (df_,  sprintf("%s/%s_mass_ENLOCALE.csv", dstock, key$state), row.names = FALSE)
-    write.csv2(df_,  sprintf("%s/%s_mass_DELOCALE.csv", dstock, key$state), row.names = FALSE)
+    write.csv (df_,  sprintf("%s/mass-per-state/%s_mass_ENLOCALE.csv", dcsv, key$state), row.names = FALSE)
+    write.csv2(df_,  sprintf("%s/mass-per-state/%s_mass_DELOCALE.csv", dcsv, key$state), row.names = FALSE)
 
 }
 
